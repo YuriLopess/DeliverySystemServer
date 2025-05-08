3a0a203d4e24b5abcf3cad277f7392b8e1a0334a
YuriLopess <costalopesyuri@gmail.com>
2025-05-08 20:40:29 -0300

    Inital Commit

---
+++ src/main/java/io/github/YuriLopess/DeliverySystemServer/DeliverySystemServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.YuriLopess.DeliverySystemServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverySystemServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/YuriLopess/DeliverySystemServer/DeliverySystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.YuriLopess.DeliverySystemServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

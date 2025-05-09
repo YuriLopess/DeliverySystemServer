87ece34029adb18fea0f0d64ed8344639d84e912
YuriLopess <costalopesyuri@gmail.com>
2025-05-08 21:49:06 -0300

    feat: config eureka server

--- src/main/java/io/github/YuriLopess/DeliverySystemServer/Application.java
+++ src/main/java/io/github/YuriLopess/DeliverySystemServer/Application.java
@@ -2,12 +2,14 @@ package io.github.YuriLopess.DeliverySystemServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class DeliverySystemServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliverySystemServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
